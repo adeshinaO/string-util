800b51f02f20e67d71cad167216177bf86a3b146
Adeshina Ogunmodede <abdulwahabogunmodede@gmail.com>
2020-10-19 15:15:42 +0100

    finished documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,68 +1,30 @@
+//! # `string_util` provides support for a small number of operations involving strings.
 
+use std::fmt::{Display, Formatter, Result as FmtResult};
 
-// TODO: Write a custom error type (ErrorKind ?) to replace 'String'... see rust by example
-
-// TODO: Skim the book chapter on Strings
-
-// Read Rust guide on API design guide.
-
-use std::fmt::{Formatter, Display, Result as FmtResult};
-use regex::Regex;
-
-// TODO: Don't just copy this. Read a short regex refresher tutorial and write the regex.
-// r""" is used for raw string. escape characters are not processed.
-const EMAIL_REGEX_PATTERN: &str = r"";
-
-#[derive(Debug, Clone)]
-pub struct EmptyStringError;
-
-
-impl Display for EmptyStringError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
-        write!(f, "The supplied string is empty")
-    }
-}
-
-pub fn is_valid_email(txt: &String) -> bool {
-
-    // IF this crate is going to be published, then this method has to be thorough
-    // Use wikipedia to figure out what to do.
-
-    // This won't work!
-    /*if !txt.is_empty() {
-        // Panic is acceptable since the Regex should never be wrong.
-        let email_pattern = Regex::new(EMAIL_REGEX_PATTERN).unwrap(); 
-        return email_pattern.is_match(txt);
-    } */
-
-    // TODO:
-    // Check if the email contains '@' and use that to split the address into username and domain.
-    // Validate username and domain separately, if both are valid then the address is valid
-
-
-    // See link for what valid email.
-    // https://en.wikipedia.org/wiki/Email_address
-
-
-    let parts: Vec<&str> = txt.split('@').collect();
-
-    if parts.len() != 2 {
-        return false;
-    }
-
-    let is_valid_username = validate_email_username(parts.get(0).unwrap());
-    let is_valid_domain = validate_email_domain(parts.get(1).unwrap());
-
-  
-    is_valid_domain && is_valid_username
-}
-
+/// Capitalizes the first character of the given string according to `cap_type`.
+/// Returns `Err(EmptyStringError)` if the string is empty.
+///
+/// # Example
+///
+/// ```
+/// use string_util::Capitalization;
+///
+/// let result = string_util::capitalize(String::from("ngozi"), Capitalization::UpperCase).unwrap();
+/// assert_eq!(result,  String::from("Ngozi"));
+///
+/// let result = string_util::capitalize(String::from("Shalewa"), Capitalization::LowerCase).unwrap();
+/// assert_eq!(result, String::from("shalewa"));
+/// ```
 pub fn capitalize(txt: String, cap_type: Capitalization) -> Result<String, EmptyStringError> {
-
+    let txt = txt.trim();
     let mut char_vec = txt.chars().collect::<Vec<char>>();
 
     if let Some(first_char) = char_vec.get_mut(0) {
-        
+        if first_char.is_whitespace() {
+            return Err(EmptyStringError);
+        }
+
         if let Capitalization::UpperCase = cap_type {
             first_char.make_ascii_uppercase();
         } else {
@@ -81,10 +43,23 @@ pub fn capitalize(txt: String, cap_type: Capitalization) -> Result<String, Empty
     }
 }
 
-
+/// Converts a vector of any `Display` type to a string delimited by the passed character.
+///
+/// # Example
+///
+/// ```
+/// use string_util;
+///
+/// let vect1 = vec!["APC".to_string(), "PDP".to_string(), "ANPP".to_string()];
+/// let vect2 = vec!["127".to_string(), "0".to_string(), "0".to_string(), "1".to_string()];
+///
+/// let delim_string_1 = string_util::vec_to_delimited_str(&vect1, ':');
+/// let delim_string_2 = string_util::vec_to_delimited_str(&vect2, '.');
+///
+/// assert!(delim_string_2.contains("127.0.0.1"));
+/// assert!(delim_string_1.contains("APC:PDP:ANPP"));
+/// ```
 pub fn vec_to_delimited_str<T: Display>(str_vec: &Vec<T>, delimiter: char) -> String {
-    
-    // If the vector is empty, return an empty string.
     let mut result = String::new();
 
     for (i, str) in str_vec.iter().enumerate() {
@@ -96,34 +71,15 @@ pub fn vec_to_delimited_str<T: Display>(str_vec: &Vec<T>, delimiter: char) -> St
 
     result
 }
+#[derive(Debug, Clone)]
+pub struct EmptyStringError;
 
+impl Display for EmptyStringError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "The supplied string is empty")
+    }
+}
 pub enum Capitalization {
     LowerCase,
-    UpperCase
-}
-
-fn validate_email_domain(domain: &str) -> bool {
-
-
-    false
-}
-
-fn validate_email_username(username: &str) -> bool {
-
-
-    false
-}
-
-
-#[cfg(test)]
-mod tests {
-
-    #[test]
-    fn checK_username_validation() {
-    
-    }
-
-    fn check_domain_validation() {
-
-    }
+    UpperCase,
 }
--- tests/int_tests.rs
+++ tests/int_tests.rs
@@ -1,60 +1,33 @@
-use string_util::{Capitalization};
-
-#[test]
-fn email_check() {
-    
-    let valid1 = "ade.32@rust.com".to_owned();
-    let valid2 = "ade@rust.com".to_owned();
-
-    // Write assertions for these.
-    let valid3 = "ade-65@rust.com".to_owned();
-    let valid4 = "ade_65@rust.com".to_owned();
-
-    let valid5 = "ade_65@".to_owned();
-    let invalid1 = "ade@com".to_owned();
-    let invalid2 = "ade@rust..com".to_owned();
-    //let invalid3 = "ade@com".to_owned();
-    let invalid4 = "ade@.com".to_owned();
-
-    // todo: write assertions for thee
-    let invalid5 = "ade..34@rust.com".to_owned();
-    let invalid6 = "ade#34@rust.com".to_owned();
-    let invalid7 = "ade__34@rust.com".to_owned();
-    let invalid8 = "ade--34@rust.com".to_owned();
-    let invalid9 = "ade.@rust.com".to_owned();
-    let invalid10 = "ade..34@rust.com".to_owned();
-    let invalid11 = ".ade@rust.com".to_owned();
-
-
-    assert!(!string_util::is_valid_email(&invalid4));
-    //assert!(!string_util::is_valid_email(&invalid3));
-    assert!(!string_util::is_valid_email(&invalid2));
-    assert!(!string_util::is_valid_email(&invalid1));
-    assert!(!string_util::is_valid_email(&valid1));
-    assert!(!string_util::is_valid_email(&valid2));
-}
+use string_util::Capitalization;
 
 #[test]
 fn capitalization() {
-
     let result = string_util::capitalize(String::from("ngozi"), Capitalization::UpperCase).unwrap();
     let expected = String::from("Ngozi");
     assert_eq!(result, expected);
-    
-    let result = string_util::capitalize(String::from("Shalewa"), Capitalization::LowerCase).unwrap();
+
+    let result =
+        string_util::capitalize(String::from("Shalewa"), Capitalization::LowerCase).unwrap();
     let expected = String::from("shalewa");
     assert_eq!(result, expected);
+
+    let result = string_util::capitalize(String::from(" "), Capitalization::LowerCase);
+    result.expect_err("The string is not empty");
 }
 
 #[test]
 fn vector_to_string() {
+    let vect1 = vec!["APC".to_string(), "PDP".to_string(), "ANPP".to_string()];
+    let vect2 = vec![
+        "127".to_string(),
+        "0".to_string(),
+        "0".to_string(),
+        "1".to_string(),
+    ];
 
-    let str_vector = vec!["APC".to_string(), "PDP".to_string(), "ANPP".to_string()];
-    let empty_vec: Vec<String> = Vec::new();
+    let delim_string_1 = string_util::vec_to_delimited_str(&vect1, ':');
+    let delim_string_2 = string_util::vec_to_delimited_str(&vect2, '.');
 
-    let delimited_string = string_util::vec_to_delimited_str(&str_vector, ',');
-    let empty_string = string_util::vec_to_delimited_str(&empty_vec, ',');
-
-    assert!(empty_string.is_empty());
-    assert!(delimited_string.contains("APC,PDP,ANPP"));
+    assert!(delim_string_2.contains("127.0.0.1"));
+    assert!(delim_string_1.contains("APC:PDP:ANPP"));
 }
